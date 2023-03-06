4eb4e094f8f5e72575de68354c1099ffcc0bde54
Toluleke Ilori <merliebelly@gmail.com>
2023-03-06 11:36:23 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 
 char *_memset(char *s, char b, unsigned int n);
@@ -18,3 +21,4 @@ void print_diagsums(int *a, int size);
 
 void set_string(char **s, char *to);
 
+#endif
